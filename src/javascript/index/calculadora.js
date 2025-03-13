cd84c457b297de204c8e1a91c87d5be0ea26c1cb
Joao Carlos <joaocarlosoffice16@gmail.com>
2025-03-13 17:13:57 -0300

    calculadora atualizada

---
+++ src/javascript/index/calculadora.js
@@ -0,0 +1,97 @@
+//app da tela de inicio
+const appAtivoCal = document.getElementById("calculadora")
+
+const calFunc = document.getElementById("calculadora-funci")
+
+//botao de voltar
+const voltarCal = document.getElementById("voltar-cal")
+
+// Tela
+const telaCal = document.getElementById("calculadora-funci-tela");
+
+// Botões de números
+const btn1 = document.getElementById("number1");
+const btn2 = document.getElementById("number2");
+const btn3 = document.getElementById("number3");
+const btn4 = document.getElementById("number4");
+const btn5 = document.getElementById("number5");
+const btn6 = document.getElementById("number6");
+const btn7 = document.getElementById("number7");
+const btn8 = document.getElementById("number8");
+const btn9 = document.getElementById("number9");
+
+// Botões de operação
+const btnMais = document.getElementById("selMais");
+const btnMenos = document.getElementById("selMenos");
+const btnVezes = document.getElementById("selVezes");
+const btnDiv = document.getElementById("selDiv");
+const btnIgual = document.getElementById("selIgual");
+
+
+function adicionar(valor) {
+    telaCal.value += valor; 
+}
+
+
+appAtivoCal.addEventListener("click", function () {
+    calFunc.style.display = "flex"
+    bt.style.display = "flex";
+
+})
+
+voltarCal.addEventListener("click", function () {
+    calFunc.style.display = "none"
+    bt.style.display = "flex";
+    document.getElementById("bateria").style.display = "flex"
+
+})
+
+
+btn1.addEventListener("click", function() {
+  adicionar(btn1.value);
+});
+btn2.addEventListener("click", function() {
+  adicionar(btn2.value);
+});
+btn3.addEventListener("click", function() {
+  adicionar(btn3.value);
+});
+btn4.addEventListener("click", function() {
+  adicionar(btn4.value);
+});
+btn5.addEventListener("click", function() {
+  adicionar(btn5.value);
+});
+btn6.addEventListener("click", function() {
+  adicionar(btn6.value);
+});
+btn7.addEventListener("click", function() {
+  adicionar(btn7.value);
+});
+btn8.addEventListener("click", function() {
+  adicionar(btn8.value);
+});
+btn9.addEventListener("click", function() {
+  adicionar(btn9.value);
+});
+
+btnMais.addEventListener("click", function() {
+  adicionar(btnMais.value);
+});
+btnMenos.addEventListener("click", function() {
+  adicionar(btnMenos.value);
+});
+btnVezes.addEventListener("click", function() {
+  adicionar(btnVezes.value);
+});
+btnDiv.addEventListener("click", function() {
+  adicionar(btnDiv.value);
+});
+
+btnIgual.addEventListener("click", function() {
+  try {
+    return telaCal.value = eval(telaCal.value); 
+  } catch (e) {
+    telaCal.value = "Erro"; 
+  }
+});
--- src/javascript/index/liga-e-desliga.js
+++ src/javascript/index/liga-e-desliga.js
@@ -2,6 +2,7 @@ const btn = document.getElementById("btn-digt");
 let tela = document.getElementById("tela");
 const ic = document.getElementById("icones");
 const icr = document.getElementById("icones-right");
+const bt = document.getElementById("bateria");
 const bl1 = document.getElementById("bloco1");
 const bl2 = document.getElementById("bloco2");
 const bl3 = document.getElementById("bloco3");
@@ -10,17 +11,19 @@ const TelaDescarregou = document.getElementById("fim-de-carga");
 const apps = document.getElementById("apps")
 function alternar() {
   if (tela.style.backgroundImage) {
-    tela.style.backgroundImage = ""; // Remove a imagem
+    tela.style.backgroundImage = ""; 
     ic.style.display = "none";
     icr.style.display = "none";
     apps.style.display = "none"
+    calFunc.style.display = "none"
 
   } else {
     tela.style.backgroundImage =
-      "url('../../../assents/img/wallpaper/wallpaper.png')"; // Define a imagem
+      "url('../../../assents/img/wallpaper/wallpaper.png')"; 
     ic.style.display = "flex";
     icr.style.display = "flex";
     bl1.style.display = "block";
+    bt.style.display = "flex";
     bl2.style.display = "block";
     bl3.style.display = "block";
     bl4.style.display = "block";
@@ -34,34 +37,35 @@ function descarregou() {
       tela.style.backgroundImage = "";
       ic.style.display = "none";
       apps.style.display = "none"
+      calFunc.style.display = "none"
 
       icr.style.display = "none";
-    }, 12000);
+    }, 12000000);
   }
 }
 
 function TelaDescarregouF() {
   setTimeout(() => {
     TelaDescarregou.style.display = "flex";
-  }, 10000);
+  }, 12000000);
   setTimeout(() => {
     TelaDescarregou.style.display = "none";
-  }, 12000);
+  }, 12500000);
 }
 
 function bateria() {
   setTimeout(() => {
     bl4.style.display = "none";
-  }, 3000);
+  }, 30000);
   setTimeout(() => {
     bl3.style.display = "none";
-  }, 6000);
+  }, 60000);
   setTimeout(() => {
     bl2.style.display = "none";
-  }, 9000);
+  }, 90000);
   setTimeout(() => {
     bl1.style.display = "none";
-  }, 11000);
+  }, 110000);
 }
 
 btn.addEventListener("click", function () {
